fef2ec1d8b6e19dc66204d21136736fa1050c608
tgavrity <92818537+tgavrity@users.noreply.github.com>
2021-11-11 09:32:51 -0800

    Add files via upload

---
+++ L01HandsOn/script.js
@@ -0,0 +1,18 @@
+alert("hello")
+
+function numberCheck(number1, number2){
+    if(number1 > 100){
+        console.log("Number 1 has many digits!");
+    }
+    if(number1 < 0){
+        console.log("Number 1 is a negative number");
+    }
+    if(number2 < 20){
+        console.log("Number 2 is not a very high number");
+    }
+    if(number2 > 50){
+        console.log("Number 2 is a high number");
+    }
+}
+numberCheck(102, 7)
+console.log(numberCheck)
